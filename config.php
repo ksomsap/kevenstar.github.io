8a3facfb67b8c08a27351520bd733cdec0ad2521
ksomsap <58353745+ksomsap@users.noreply.github.com>
2023-04-08 20:54:37 +0700

    Create config.php
    Signed-off-by: ksomsap <58353745+ksomsap@users.noreply.github.com>

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$db_host = "us-cdbr-east-06.cleardb.net";
+$db_user = "b50e44413327ab";
+$db_pass = "b94d7efa";
+$db_name = "heroku_3946805a047edb8";
+
+$connect = mysqli_connect($db_host,$db_user,$db_pass,$db_name) or die("database connection error");
